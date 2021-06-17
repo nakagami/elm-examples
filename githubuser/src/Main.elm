2d84ac7d068421acf9efcffdbfca709f0d6865e4
Hajime Nakagami <nakagami@gmail.com>
2021-06-17 12:52:58 +0900

    add githubuser initial file

---
+++ githubuser/src/Main.elm
@@ -0,0 +1,113 @@
+module Main exposing (..)
+
+-- https://github.com/nakagami/elm-examples/tree/master/githubzen
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+
+
+
+-- MAIN
+
+
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+type Model
+    = Failure
+    | Loading
+    | Success String
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Loading, getGithubZen )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = MorePlease
+    | GotZen (Result Http.Error String)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        MorePlease ->
+            ( Loading, getGithubZen )
+
+        GotZen result ->
+            case result of
+                Ok principle ->
+                    ( Success principle, Cmd.none )
+
+                Err _ ->
+                    ( Failure, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h2 [] [ text "Github Zen" ]
+        , viewZen model
+        ]
+
+
+viewZen : Model -> Html Msg
+viewZen model =
+    case model of
+        Failure ->
+            div []
+                [ text "I could not load a random cat for some reason. "
+                , button [ onClick MorePlease ] [ text "Try Again!" ]
+                ]
+
+        Loading ->
+            text "Loading..."
+
+        Success principle ->
+            div []
+                [ button [ onClick MorePlease, style "display" "block" ] [ text "More Please!" ]
+                , text principle
+                ]
+
+
+
+-- HTTP
+
+
+getGithubZen : Cmd Msg
+getGithubZen =
+    Http.get
+        { url = "https://api.github.com/users/nakagami"
+        , expect = Http.expectString GotZen
+        }
