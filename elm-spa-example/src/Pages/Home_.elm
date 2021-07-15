53948954925ca52dec29f17fe05867f7a34484f4
Hajime Nakagami <nakagami@gmail.com>
2021-07-15 09:16:21 +0900

    add static page and navbar

--- elm-spa-example/src/Pages/Home_.elm
+++ elm-spa-example/src/Pages/Home_.elm
@@ -1,11 +1,12 @@
 module Pages.Home_ exposing (view)
 
 import Html
+import UI
 import View exposing (View)
 
 
 view : View msg
 view =
     { title = "Homepage"
-    , body = [ Html.text "Hello, world!" ]
+    , body = UI.layout [ Html.text "Hello, world!" ]
     }
